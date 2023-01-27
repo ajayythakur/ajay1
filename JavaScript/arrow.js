03220d1e9812b63f55b8a4b74e461fe63a88a17a
ajayythakur <ajay1270panwar@gmail.com>
2023-01-27 21:19:38 +0530

    blog

--- JavaScript/arrow.js
+++
@@ -1,7 +0,0 @@
-let Arrow=(a,b,c,d,e)=> {
-    var Sum=a+b+c+d+e;
-    console.log(Sum)
-    return Sum;
-}
-Arrow(1,2,3,4,5)
-
--- JavaScript/dom.js
+++
@@ -1,9 +0,0 @@
-let headel=document.getElementById("heading")
-let btnel=document.getElementById("btn")
-btnel.addEventListener("click" ,function(){
-    headel.style.color="red";
-    headel.style.fontFamily="sans serif";
-    headel.style.fontSize="30px";
-
-
-})
